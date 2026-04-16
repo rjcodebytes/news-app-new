d80d21e2799353b2131dbe4be4b03988849ea9c7
rjcodebytes <136474535+rjcodebytes@users.noreply.github.com>
2026-04-16 22:56:25 +0530

    issue fix

--- server.js
+++ server.js
@@ -11,28 +11,28 @@ mongoose.connect("mongodb://localhost:27017/newsdb");
 
 // Schema
 const newsSchema = new mongoose.Schema({
-  title: String,
-  description: String
+    title: String,
+    description: String
 });
 
 const News = mongoose.model("News", newsSchema);
 
 // POST - Add news
 app.post("/news", async (req, res) => {
-  const { title, description } = req.body;
+    const { title, description } = req.body;
 
-  const news = new News({ title, description });
-  await news.save();
+    const news = new News({ title, description });
+    await news.save();
 
-  res.json({ message: "News added successfully" });
+    res.json({ message: "News added successfully" });
 });
 
 // GET - Fetch news
 app.get("/news", async (req, res) => {
-  const data = await News.find();
-  res.json(data);
+    const data = await News.find();
+    res.json(data);
 });
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+app.listen(3000, "0.0.0.0", () => {
+    console.log("Server running on http://localhost:3000");
 });
